60767fe6c65217726739f29d852a848c92a7c44e
randomflamingguy <randomflamingguy1@gmail.com>
2024-11-08 14:55:40 +0600

    file handle

---
+++ main.py
@@ -0,0 +1,8 @@
+file1 = open('codingal.txt', 'r')
+file2 = open('codingalupdated.txt', 'w')
+for line in file1.readlines():
+    if not (line.startswith('Coding')):
+        print(line)
+        file2.write(line)
+file2.close
+file1.close
